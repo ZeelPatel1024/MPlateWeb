bc21a8bdfe6daa57e9f9fbf06b92c94e9a5eb197
Zeel Patel <patelzeel1024@gmail.com>
2025-01-06 18:05:06 -0600

    Initial commit

---
+++ Backend/src/main/java/com/MPlateWeb/MPlateWeb/MPlateWebApplication.java
@@ -0,0 +1,13 @@
+package com.MPlateWeb.MPlateWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPlateWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MPlateWebApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/MPlateWeb/MPlateWeb/MPlateWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MPlateWeb.MPlateWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPlateWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
